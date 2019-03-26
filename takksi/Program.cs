1d6217b326bb1c7eb890cf5c798cea5165134e05
gunawan <gunawan@mail.com>
2019-03-26 11:53:09 +0700

    class & object

---
+++ takksi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace takksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi baru = new taxi();
+
+            Console.Write("Masukkan Nama Driver\t: ");
+            baru.DriverName = Console.ReadLine();
+            Console.Write("Masukkan Status \t: ");
+            baru.OnDuty = bool.Parse(Console.ReadLine());
+            Console.Write("Masukkan Nomor Passenger: ");
+            baru.NumPassenger = Console.ReadLine();
+
+            baru.TaxiInfo();
+            baru.PickUpPassenger();
+            baru.DropOffPassenger();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ takksi/taxi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace takksi
+{
+    class taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public string NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("\n\nDriver Name\t\t: {0}", DriverName);
+
+           /* if (OnDuty == true)
+            {*/
+                Console.WriteLine("Status\t\t\t: {0}", OnDuty ? "Ready" : "Free");
+            /*}
+            else
+            {
+                Console.WriteLine("Status\t\t\t: Free");
+            }*/
+
+            Console.WriteLine("Number Of Passenger\t: {0}", NumPassenger);
+
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n\n{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
